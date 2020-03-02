7961eed86e516e958deef036910370814cc799da
Michal Rozycki (MYR) <MYR@kmd.dk>
2020-03-02 12:55:02 +0100

    First simple exercises.

--- myFirstPythonProgram.py
+++ myFirstPythonProgram.py
@@ -1,1 +1,32 @@
-print('hello again')
+#print('hello again')
+
+""" a = 2
+b = 2
+c = (++a + ++a)
+ile wyniesie c?
+print(c) """
+
+""" print("Twinkle, twinkle, little star,")
+print(" "," "," "," "," ", 'How I wonder that you are!')
+print(" "," "," "," "," "," "," "," "," ",'Up above the world so high,')
+print(" "," "," "," "," "," "," "," "," ",'Like a diamond in the sky.')
+print("Twinkle, twinkle, little star,")
+print(" "," "," "," "," ", 'How I wonder that you are') """
+
+""" print("A\n","A\n\n","A\n\n\n","A\n\n\n\n","B")  """
+
+""" print("Twinkle, twinkle, little star, \n\tHow I wonder that you are! \n\t\tUp above the world so high, \n\t\tLike a diamond in the sky.\nTwinkle, twinkle, little star, \n\tHow I wonder that you are!") """
+
+""" import sys
+import time
+print("Python version")
+time.sleep(5)
+print(sys.version)
+print("Version Info.")
+print(sys.version_info)
+#input('Waiting a key...') """
+
+import datetime
+now = datetime.datetime.now()
+print("Current date and time: ")
+print(now.strftime("%Y-%m-%d %H:%M:%S"))
