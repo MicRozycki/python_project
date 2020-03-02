deaa4286000ef97c1a54b375977ba8b1f52d3043
Michal Rozycki <MYR@kmd.dk>
2020-03-02 17:20:32 +0100

    continue of exercises

--- myFirstPythonProgram.py
+++ myFirstPythonProgram.py
@@ -26,7 +26,7 @@ print("Version Info.")
 print(sys.version_info)
 #input('Waiting a key...') """
 
-import datetime
+""" import datetime
 now = datetime.datetime.now()
 print("Current date and time: ")
-print(now.strftime("%Y-%m-%d %H:%M:%S"))
+print(now.strftime("%Y-%m-%d %H:%M:%S")) """
